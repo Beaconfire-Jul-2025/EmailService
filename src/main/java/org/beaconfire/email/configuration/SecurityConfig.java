34b606616d5b8918f975bca66e475444692788d3
VulpesVulpes825 <vulpesvulpes825@gmail.com>
2025-07-13 08:27:21 -0700

    feat(security): add spring security and use header from API gateway service to do auth (#5)

---
+++ src/main/java/org/beaconfire/email/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.beaconfire.email.configuration;
+
+import lombok.AllArgsConstructor;
+import org.beaconfire.email.filter.HeaderAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
+public class SecurityConfig {
+  private HeaderAuthenticationFilter headerAuthenticationFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+        .addFilterBefore(headerAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable);
+    return http.build();
+  }
+}
--- src/main/java/org/beaconfire/email/controller/EmailController.java
+++ src/main/java/org/beaconfire/email/controller/EmailController.java
@@ -1,27 +1,24 @@
 package org.beaconfire.email.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.beaconfire.email.model.EmailMessage;
 import org.beaconfire.email.service.EmailPublisher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/email")
+@RequiredArgsConstructor
+@Slf4j
 public class EmailController {
 
   private final EmailPublisher emailPublisher;
 
-  @Autowired
-  public EmailController(EmailPublisher emailPublisher) {
-    this.emailPublisher = emailPublisher;
-  }
-
   @PostMapping("/send")
   public ResponseEntity<String> sendEmail(@RequestBody EmailMessage emailMessage) {
+    log.info("Received email request: {}", emailMessage);
     emailPublisher.sendEmail(emailMessage);
     return ResponseEntity.ok("Email request sent to queue");
   }
